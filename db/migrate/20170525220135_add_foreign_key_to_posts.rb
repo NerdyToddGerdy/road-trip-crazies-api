06a9582d59b44b4d81b4a8123d3cb764cd6ab9b1
Todd Gerdy <toddgerdy@Todds-iMac.local>
2017-05-25 21:09:36 -0400

    fixed the few bugs

--- db/migrate/20170525220135_add_foreign_key_to_posts.rb
+++ db/migrate/20170525220135_add_foreign_key_to_posts.rb
@@ -1,5 +1,5 @@
 class AddForeignKeyToPosts < ActiveRecord::Migration[5.0]
   def change
-     add_column :posts, :user_id, :string
+     add_column :posts, :user_id, :integer
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170525132942) do
+ActiveRecord::Schema.define(version: 20170525220135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 20170525132942) do
     t.string   "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "user_id"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
